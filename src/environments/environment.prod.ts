3cdea35d5f327a5a5227584b73cf252bfaee3eec
Joshmyles <joshmyles844@gmail.com>
2024-06-04 08:49:17 +0300

    Project setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'https://webapi.telaschool.org',
+  LOGGED_USER_PROFILE: 'uewyioj2wodcn-ew',
+  Auth_TOKEN_KEY: 'wertedscr-2ewdfvrf',
+};
